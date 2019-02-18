57132b74900e4b10dd96f075ed0648b695bd5c4a
ghr85 <ghrenfrew@gmail.com>
2019-02-18 17:05:32 +0000

    Scenario 4 completed

--- whodunnit.js
+++ whodunnit.js
@@ -1,18 +1,18 @@
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
 
-let murderer = 'Professor Plum';
-
-const declareMurderer = function() {
-  let murderer = 'Mrs. Peacock';
-  return `The murderer is ${murderer}.`;
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
 }
 
-const firstVerdict = declareMurderer();
-console.log('First Verdict: ', firstVerdict);
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
 
-const secondVerdict = `The murderer is ${murderer}.`;
-console.log('Second Verdict: ', secondVerdict);
-
-// In this example the first murderer will be Mrs Peacock as the firstVerdict constant 
-// references a function whose murderer scope is within that function, The secondVerdict
-// will be that of professor plum as the murderer variable referenced is outside the scope
-// of the declaremurder constant
+// In this scenario there will be two separate outputs firstly implicating Miss Scarlet, 
+// Professor Plum & colonel mustard as number 3 as the constant suspects calls the function
+// declare all suspects where suspect 3 has been changed within its limited block scope.
+// The second output will identify suspect three as Mrs Peacock as suspect 3 remains
+// unchanged outside the block
--- whodunnit_instructions.md
+++ whodunnit_instructions.md
@@ -84,6 +84,8 @@ const suspects = declareAllSuspects();
 console.log(suspects);
 console.log(`Suspect three is ${suspectThree}.`);
 ```
+*In this scenario there will be two separate outputs firstly implicating Miss Scarlet, Professor Plum & colonel mustard as number 3 as the constant suspects calls the function declare all suspects where suspect 3 has been changed within its limited block scope. The second output will identify suspect three as Mrs Peacock as suspect 3 remains unchanged outside the block*
+
 
 #### Episode 5
 
